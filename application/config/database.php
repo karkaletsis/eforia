3386e7438bb44a016556b157b754602d5ace63a2
karkaletsis <k.karkaletsis@gnomon.com.gr>
2013-11-25 23:29:44 +0200

    database config

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'em1ge0r';
+$db['default']['password'] = 'changeit';
 $db['default']['database'] = 'eforia';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
